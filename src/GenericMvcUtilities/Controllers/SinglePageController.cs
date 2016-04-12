65c18279e82fe16c62156e0418ae9166eaea6306
Joshua Clark <clarkis117@live.com>
2016-04-12 07:04:22 -0400

    incremental

---
+++ src/GenericMvcUtilities/Controllers/SinglePageController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenericMvcUtilities.Controllers
+{
+    public class SinglePageController
+    {
+    }
+}
---
+++ src/GenericMvcUtilities/ViewModels/SinglePageApp/Page.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenericMvcUtilities.ViewModels.SinglePageApp
+{
+	public class Page
+	{
+		public string Id { get; set; }
+
+		public string Name { get; set; }
+
+		public virtual string ViewPath
+		{
+			get { return ($"~/Views/.cshtml"); }
+		}
+
+		public virtual string View { get; set; }
+
+		public Page()
+		{
+
+		}
+
+	}
+
+	public class Editor
+	{
+
+	}
+
+	public class GraphEditor
+	{
+
+	}
+}
